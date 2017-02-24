65351791e37e34c1b541ad26a4ebeb148e99e926
Mario Schüttel <lxxxvi@users.noreply.github.com>
2017-02-24 14:05:50 +0100

    Simplify simpli find (#4)
    * WIP, test failing
    
    * change from ./bin/simply.sh to ./simply

--- test/simpli/script_finder_test.rb
+++ test/simpli/script_finder_test.rb
@@ -7,22 +7,22 @@ module Simpli
     ALPHA_PROJECT_DIR = "#{TEST_PROJECTS_DIR}/alpha".freeze
     BRAVO_PROJECT_DIR = "#{TEST_PROJECTS_DIR}/bravo".freeze
 
-    SIMPLY_PATH_IN_ALPHA = "#{ALPHA_PROJECT_DIR}/#{Simpli::ScriptFinder::SIMPLY_SCRIPT_RELATIVE_PATH}".freeze
+    SIMPLY_PATH_IN_ALPHA = "#{ALPHA_PROJECT_DIR}/./simply".freeze
 
     def test_alpha_project_base_directory
-      assert_equal SIMPLY_PATH_IN_ALPHA, Simpli::ScriptFinder.find(ALPHA_PROJECT_DIR)
+      assert_equal SIMPLY_PATH_IN_ALPHA, Simpli::locate_script('./simply', ALPHA_PROJECT_DIR)
     end
 
     def test_alpha_project_inner_directory
-      assert_equal SIMPLY_PATH_IN_ALPHA, Simpli::ScriptFinder.find("#{ALPHA_PROJECT_DIR}/some/other/directory")
+      assert_equal SIMPLY_PATH_IN_ALPHA, Simpli::locate_script('./simply', "#{ALPHA_PROJECT_DIR}/some/other/directory")
     end
 
     def test_bravo_project_base_directory
-      assert_nil Simpli::ScriptFinder.find(BRAVO_PROJECT_DIR)
+      assert_nil Simpli::locate_script('./simply', BRAVO_PROJECT_DIR)
     end
 
     def test_bravo_project_inner_directory
-      assert_nil Simpli::ScriptFinder.find("#{BRAVO_PROJECT_DIR}/some/other/directory")
+      assert_nil Simpli::locate_script('./simply', "#{BRAVO_PROJECT_DIR}/some/other/directory")
     end
   end
 end
